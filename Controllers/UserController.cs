5d43fa54ae3864d6314b3cc5f0700d8810a2beff
Umi5 <manubusqui@gmail.com>
2024-03-25 13:32:50 +0100

    add register and login for user

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -12,5 +12,33 @@ namespace net_auth_api.Controllers
     public class UserController : ControllerBase
     {
         public static User user = new User();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request){
+            string passwordHash = 
+                BCrypt.Net.BCrypt.HashPassword(request.Pasword);
+
+            user.Username = request.Username;
+            user.PaswordHash = passwordHash;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<User> Login(UserDto request){
+            
+            if(user.Username != request.Username ) {
+                return BadRequest("User not found");
+            }
+
+            if(!BCrypt.Net.BCrypt.Verify(request.Pasword, user.PaswordHash)){
+                return BadRequest("Wrong password");
+            }
+
+            
+
+            return Ok(user);
+        }
+
     }
 }
